a9edc8cdb0e989f7add4afbc671470a9d2c76505
Yuri <36759251+yurapivnenko@users.noreply.github.com>
2018-05-08 19:43:23 +0300

    Initial commit

---
+++ controllers/NewsController.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once ROOT.'/models/News.php';
+
+class NewsController
+{
+    public function actionIndex()
+    {
+        $newsList = array();
+        $newsList = News::getNewsList();
+
+        echo '<pre>';
+        print_r($newsList);
+        echo '</pre>';
+    }
+
+    public function actionView($id)
+    {
+        if ($id) {
+            $newsItem = News::getNewsItemById($id);
+
+            echo '<pre>';
+            print_r($newsItem);
+            echo '</pre>';
+        }
+    }
+}
