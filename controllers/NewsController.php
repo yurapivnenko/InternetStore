173bd417c5a4d88ebbfa7f0f198e0aa9cd903067
Yuri <36759251+yurapivnenko@users.noreply.github.com>
2018-05-15 22:33:34 +0300

    commit

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,8 +4,6 @@ include_once(ROOT.'/models/News.php');
 
 class NewsController
 {
-<<<<<<< HEAD
-=======
     public function actionIndex()
     {
         $newsList = array();
@@ -18,7 +16,6 @@ class NewsController
         return true;
     }
 
->>>>>>> ba462ecf45785a5e64a132f21f4df3907f0c3c4f
     public function actionView($id)
     {
         if ($id) {
@@ -30,15 +27,5 @@ class NewsController
         }
 
         return true;
-    }
-
-    public function actionIndex()
-    {
-        $newsList = array();
-        $newsList = News::getNewsList();
-
-        echo '<pre>';
-        print_r($newsList);
-        echo '</pre>';
     }
 }
