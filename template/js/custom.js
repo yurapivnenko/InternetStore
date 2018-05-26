4d3c4026c96bdcc02ad7ad14e43594928b7adcdf
Yuri <36759251+yurapivnenko@users.noreply.github.com>
2018-05-26 18:23:23 +0300

    commit

--- template/js/custom.js
+++ template/js/custom.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("navbar").style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("navbar").style.backgroundColor = "#e3f2fd";
 }
